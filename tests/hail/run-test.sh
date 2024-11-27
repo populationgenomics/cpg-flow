12fa5bd351a8f8344fc4f697c8bc71f328f48a25
Sabrina Yan <9669990+violetbrina@users.noreply.github.com>
2024-11-27 15:13:54 +1100

    test(tests/hail/config.toml,tests/hail/run-test.sh): add warnings

--- tests/hail/run-test.sh
+++ tests/hail/run-test.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+# Fail with warning if the most recent local commit is not pushed to the remote
+if [ -n "$(git status --porcelain)" ]; then
+    echo "There are uncommitted changes in the repository. Please commit and push them before running the tests."
+    exit 1
+fi
+
+# Check that the most recent commit has been pushed to the remote
+if [ -n "$(git log origin/main..HEAD)" ]; then
+    echo "There are unpushed commits in the repository. Please push them before running the tests."
+    exit 1
+fi
+
+# Get commit sha
+COMMIT_SHA=$(git rev-parse HEAD)
+IMAGE_NAME="australia-southeast1-docker.pkg.dev/cpg-common/images-tmp/cpg_flow:$COMMIT_SHA"
+
 analysis-runner \
+    --image "$IMAGE_NAME" \
     --dataset "fewgenomes" \
     --description "cpg-flow_test" \
     --access-level "test" \
     --output-dir "cpg-flow_test" \
-    --config "tests/hail/config.toml" \
-    tests/hail/workflow.py
+    --config "config.toml" \
+    workflow.py
